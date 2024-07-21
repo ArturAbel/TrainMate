f8b4811f590dcb84432b102a0ecec2bca2937b45
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2024-07-21 19:15:29 +0300

    Done with making the user doc object without messages

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,21 +5,8 @@ import { HomeDivider } from "../../components/HomeDivider/HomeDivider";
 import { HomeHero } from "../../components/HomeHero/HomeHero";
 
 import "./Home.css";
-import { fetchUsers } from "../../redux/features/usersSlice";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
 
 export const Home = () => {
-
-  const dispatch = useDispatch();
-  const { users } = useSelector((state) => state.users);
-
-   useEffect(() => {
-    
-    dispatch(fetchUsers())
-   }, []);
-   
-   console.log(users);
   return (
     <section className="home-page">
       <HomeHero />
