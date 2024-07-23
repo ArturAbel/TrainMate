86f0e362f088caef3b690d50b38628f3f0ab40f6
nahum22 <nahum.nahum@gmail.com>
2024-07-23 16:39:10 +0300

    merge comments

--- src/components/TrainerCard/TrainerCard.jsx
+++ src/components/TrainerCard/TrainerCard.jsx
@@ -7,8 +7,6 @@ import { Link } from "react-router-dom";
 
 import { FaHeart } from "react-icons/fa6";
 import "./TrainerCard.css";
-
-import { useSelector, useDispatch } from "react-redux";
 import { useState } from "react";
 import {
   addFavorite,
