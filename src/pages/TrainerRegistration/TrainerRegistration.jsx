8164d862db12eef6d36032e6f894052f8fc6d7ea
Artur Abel <163734294+ArturAbel@users.noreply.github.com>
2024-07-24 12:17:48 +0300

    working on trainer registration page

--- src/pages/TrainerRegistration/TrainerRegistration.jsx
+++ src/pages/TrainerRegistration/TrainerRegistration.jsx
@@ -1,10 +1,45 @@
+import { BsHandThumbsDown, BsHandThumbsUp } from "react-icons/bs";
+import { sports } from "../../utilities/constants";
+
 import "./TrainerRegistration.css";
 
 export const TrainerRegistration = () => {
   return (
     <section className="trainer-registration-section">
-      <div className="trainer-registration-rules-container"></div>
-      <div className="trainer-registration-form-container"></div>
+      <div className="trainer-registration-instructions-container">
+        <div>
+          <h1 className="trainer-registration-instructions-title">
+            instructions
+          </h1>
+          <p className="trainer-registration-instructions-explanation">
+            explanation about the process
+          </p>
+        </div>
+        <div className="trainer-registration-instructions-dos">
+          <BsHandThumbsUp />
+          dos
+        </div>
+        <div className="trainer-registration-instructions-dos">
+          <BsHandThumbsDown />
+          dont&apos;s
+        </div>
+      </div>
+      <div className="trainer-registration-form-container">
+        <form action="" className="trainer-registration-form">
+          <div className="trainer-registration-input-container">
+            <label className="trainer-registration-form-label">
+              choose your sport
+            </label>
+            <select className="trainer-registration-form-label" name="sport">
+              {sports.map((sport, index) => (
+                <option key={index} value={sport}>
+                  {sport}
+                </option>
+              ))}
+            </select>
+          </div>
+        </form>
+      </div>
     </section>
   );
 };
