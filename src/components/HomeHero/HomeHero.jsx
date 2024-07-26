e51b31a355e482257c7f3d2dcd2b3929265e22c9
Artur Abel <163734294+ArturAbel@users.noreply.github.com>
2024-07-26 14:43:21 +0300

    hero section css

--- src/components/HomeHero/HomeHero.jsx
+++ src/components/HomeHero/HomeHero.jsx
@@ -1,10 +1,12 @@
 import { parallaxEffect } from "../../utilities/parallaxEffect";
+import { heroImage } from "../../utilities/constants";
 import { ButtonHome } from "../ButtonHome/ButtonHome";
+import { FaCircleArrowUp } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 import { useGSAP } from "@gsap/react";
 import { useRef } from "react";
 
 import "./HomeHero.css";
-import { Link } from "react-router-dom";
 
 export const HomeHero = () => {
   // Parallax Effect
@@ -12,6 +14,7 @@ export const HomeHero = () => {
   const handleMouseMove = (e) => {
     parallaxEffect(e, ".parallax-effect", containerReference, -80);
     parallaxEffect(e, ".home-hero-title", containerReference, 20);
+    parallaxEffect(e, ".hero-button", containerReference, -10);
   };
   useGSAP(
     () => {
@@ -23,12 +26,28 @@ export const HomeHero = () => {
 
   return (
     <section ref={containerReference} className="home-hero-section">
-      {/* Change names and design the div classes */}
-      <div className="parallax-effect div div-1">data</div>
-      <div className="parallax-effect div div-2">image</div>
-      <div className="parallax-effect div div-3">data</div>
+      <div className="parallax-effect hero-div hero-div-1">
+        <h2 className="hero-div-1-text">
+          Users show an improved level of dedication.
+        </h2>
+        <h2 className="hero-div-1-number">+23%</h2>
+        <div className="hero-div-1-bars">
+          <div className="hero-bar bar-1"></div>
+          <div className="hero-bar bar-2"></div>
+          <div className="hero-bar bar-3"></div>
+          <div className="hero-bar bar-4"></div>
+        </div>
+      </div>
+      <div className="parallax-effect hero-div hero-div-2">
+        <h2 className="hero-div-2-text">trainings crafted for you</h2>
+        <img className="hero-image" src={heroImage} alt="image" />
+      </div>
+      <div className="parallax-effect hero-div hero-div-3">
+        <h2 className="hero-div-3-text">fastest growing sports community</h2>
+        <FaCircleArrowUp className="hero-div-3-icon" />
+      </div>
       <h1 className="home-hero-title">
-        Elevate your game with the{" "}
+        Elevate your game with the
         <span className="title-span">ultimate sports</span> training.
       </h1>
       <Link to={"get-started"}>
