f4d32c4ab3114f5a666788c0690665d472927dc8
Artur Abel <163734294+ArturAbel@users.noreply.github.com>
2024-07-27 19:12:14 +0300

    working on trainer delete

--- src/pages/TrainerSettings/TrainerSettings.jsx
+++ src/pages/TrainerSettings/TrainerSettings.jsx
@@ -1,43 +1,41 @@
 import ProfileImageUploader from "../../components/ProfileImageUploader/ProfileImageUploader";
+import DeleteAccountModal from "../../components/DeleteAccountModal/DeleteAccountModal";
 import { HomeDivider } from "../../components/HomeDivider/HomeDivider";
+import { anonymousImage, sports } from "../../utilities/constants";
+import { deleteUserAccount } from "../../redux/features/authSlice";
 import { useDispatch, useSelector } from "react-redux";
-import { sports } from "../../utilities/constants";
+import { useFormHook } from "../../hooks/useFormHook";
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router";
 import {
+  uploadTrainerProfileImage,
   fetchTrainers,
   updateTrainer,
-  uploadTrainerProfileImage,
+  deleteTrainer,
 } from "../../redux/features/trainerSlice";
 
 import "./TrainerSettings.css";
 
 const TrainerSettings = () => {
   const { trainers, loading, error } = useSelector((state) => state.trainer);
+  const { input, setInput, handleInputChange } = useFormHook();
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const { user } = useSelector((state) => state.auth);
-  console.log(user);
   const dispatch = useDispatch();
-  const [formData, setFormData] = useState({
-    lessonLength: "",
-    description: "",
-    address: "",
-    sport: "",
-    about: "",
-    level: [],
-    price: "",
-    name: "",
-  });
+  const navigate = useNavigate();
 
-  useEffect(() => {
-    dispatch(fetchTrainers());
-  }, [dispatch]);
-
+  // Find the trainer
   const trainerData = trainers.find(
     (trainerObj) => trainerObj.uid === user.uid
   );
 
   useEffect(() => {
-    if (trainerData) {
-      setFormData({
+    dispatch(fetchTrainers());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (trainerData && user) {
+      setInput({
         lessonLength: trainerData.lessonLength || "",
         description: trainerData.description || "",
         address: trainerData.address || "",
@@ -50,45 +48,58 @@ const TrainerSettings = () => {
     }
   }, [trainerData]);
 
-  const handleImageChange = (e) => {
-    const file = e.target.files[0];
-    if (file) {
-      dispatch(uploadTrainerProfileImage(file, trainerData.uid));
-    }
-  };
+  // Delete trainer modal
+  const handleDeleteModal = () =>
+    setIsModalOpen((previousState) => !previousState);
 
-  const handleInputChange = (e) => {
-    const { name, value, type, checked } = e.target;
-    if (type === "checkbox") {
-      setFormData((prevFormData) => ({
-        ...prevFormData,
-        level: checked
-          ? [...prevFormData.level, value]
-          : prevFormData.level.filter((level) => level !== value),
-      }));
-    } else {
-      setFormData({ ...formData, [name]: value });
+  const handleDeleteAccount = async () => {
+    try {
+      dispatch(deleteTrainer(user.uid));
+      dispatch(deleteUserAccount());
+      handleDeleteModal();
+      navigate("/");
+    } catch (error) {
+      console.error("Error deleting account: ", error);
     }
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (trainerData) {
-      dispatch(updateTrainer(trainerData.uid, formData));
+      dispatch(updateTrainer(trainerData.uid, input));
     }
   };
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      dispatch(uploadTrainerProfileImage(file, user.uid));
+    }
+  };
+
+  const profileImageUrl =
+    trainerData.image ||
+    (user.providerData &&
+    user.providerData.length > 0 &&
+    user.providerData[0].providerId === "google.com"
+      ? user.photoURL
+      : anonymousImage);
 
   if (!trainerData || loading) {
     return <div>Loading trainer data...</div>;
   }
 
-  const profileImageUrl =
-    trainerData.image ||
-    (user && user.photoURL ? user.photoURL : "/public/person1.jpg");
-
   return (
     <>
       <section className="trainer-settings-section">
+        <div className="account-settings-navbar">
+          <button
+            className="account-settings-link-button"
+            onClick={handleDeleteModal}
+          >
+            Delete Account
+          </button>
+        </div>
         <div className="trainer-settings-container">
           <h1 className="trainer-settings-title">Trainer Settings</h1>
           <form className="trainer-settings-form" onSubmit={handleSubmit}>
@@ -104,7 +115,7 @@ const TrainerSettings = () => {
               <input
                 onChange={handleInputChange}
                 className="form-input"
-                value={formData.name}
+                value={input.name}
                 type="text"
                 name="name"
               />
@@ -113,7 +124,7 @@ const TrainerSettings = () => {
               <label className="form-label">Choose Your Sport</label>
               <select
                 onChange={handleInputChange}
-                value={formData.sport}
+                value={input.sport}
                 className="form-input"
                 name="sport"
               >
@@ -129,7 +140,7 @@ const TrainerSettings = () => {
               <input
                 placeholder="Enter a short description."
                 onChange={handleInputChange}
-                value={formData.description}
+                value={input.description}
                 className="form-input"
                 name="description"
                 type="text"
@@ -141,7 +152,7 @@ const TrainerSettings = () => {
                 placeholder="Tell about yourself"
                 onChange={handleInputChange}
                 className="trainer-setting-form-textarea"
-                value={formData.about}
+                value={input.about}
                 name="about"
               />
             </div>
@@ -150,7 +161,7 @@ const TrainerSettings = () => {
               <input
                 onChange={handleInputChange}
                 placeholder="Ex: Tel Aviv"
-                value={formData.address}
+                value={input.address}
                 className="form-input"
                 name="address"
                 type="text"
@@ -168,7 +179,7 @@ const TrainerSettings = () => {
                 ].map((level) => (
                   <div className="form-checkbox-container" key={level}>
                     <input
-                      checked={formData.level.includes(level)}
+                      checked={input.level?.includes(level)}
                       onChange={handleInputChange}
                       className="form-checkbox"
                       type="checkbox"
@@ -185,7 +196,7 @@ const TrainerSettings = () => {
             <div className="form-group">
               <label className="form-label">Lesson Length (minutes)</label>
               <input
-                value={formData.lessonLength}
+                value={input.lessonLength}
                 onChange={handleInputChange}
                 placeholder="In minutes"
                 className="form-input"
@@ -199,7 +210,7 @@ const TrainerSettings = () => {
               <label className="form-label">Price per Lesson (₪)</label>
               <input
                 onChange={handleInputChange}
-                value={formData.price}
+                value={input.price}
                 className="form-input"
                 placeholder="In ₪"
                 type="number"
@@ -218,6 +229,27 @@ const TrainerSettings = () => {
           </form>
         </div>
       </section>
+      <DeleteAccountModal isOpen={isModalOpen} onClose={handleDeleteModal}>
+        <h2 className="account-delete-title">
+          Are you sure you want to delete your account?
+        </h2>
+        <div className="account-delete-buttons-container">
+          <button
+            className="button-transparent"
+            onClick={handleDeleteModal}
+            id="account-delete-cancel"
+          >
+            Cancel
+          </button>
+          <button
+            className="button-transparent"
+            onClick={handleDeleteAccount}
+            id="account-delete-confirm"
+          >
+            Confirm
+          </button>
+        </div>
+      </DeleteAccountModal>
       <HomeDivider />
     </>
   );
