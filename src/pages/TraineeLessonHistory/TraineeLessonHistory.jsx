5236348fcb9ee03263e666f6ca4df18743c37841
vailercodder <shaharbk1234@gmail.com>
2024-07-30 13:33:46 +0300

    added lesson history for trainer, yay

--- src/pages/TraineeLessonHistory/TraineeLessonHistory.jsx
+++ src/pages/TraineeLessonHistory/TraineeLessonHistory.jsx
@@ -1,15 +1,23 @@
-import { useParams } from 'react-router';
-import './TraineeLessonHistory.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect, useState } from 'react';
-import { fetchUsers } from '../../redux/features/usersSlice';
-import { fetchTrainers } from '../../redux/features/trainerSlice';
+import { useParams } from "react-router";
+import "./TraineeLessonHistory.css";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { fetchUsers } from "../../redux/features/usersSlice";
+import { fetchTrainers } from "../../redux/features/trainerSlice";
 
 const TraineeLessonHistory = () => {
   const { traineeId } = useParams();
   const dispatch = useDispatch();
-  const { users, loading: loadingUsers, error: errorUsers } = useSelector((state) => state.users);
-  const { trainers, loading: loadingTrainers, error: errorTrainers } = useSelector((state) => state.trainer);
+  const {
+    users,
+    loading: loadingUsers,
+    error: errorUsers,
+  } = useSelector((state) => state.users);
+  const {
+    trainers,
+    loading: loadingTrainers,
+    error: errorTrainers,
+  } = useSelector((state) => state.trainer);
   const [traineeHistory, setTraineeHistory] = useState([]);
   const [trainersData, setTrainersData] = useState({});
 
@@ -51,13 +59,23 @@ const TraineeLessonHistory = () => {
         <h1 className="lesson-history-title">Your booked lesson History</h1>
         <div className="trainee-lesson-history-items">
           {traineeHistory.map((lesson) => {
-            const trainer = trainersData[lesson.trainerId]; 
+            const trainer = trainersData[lesson.trainerId];
             return (
-              <div className="trainee-lesson-history-item" key={`${lesson.userId}-${lesson.date}-${lesson.hour}`}>
+              <div
+                className="trainee-lesson-history-item"
+                key={`${lesson.userId}-${lesson.date}-${lesson.hour}`}
+              >
                 <div className="lesson-trainer-content">
                   {trainer ? (
                     <>
-                      <img src={trainer.image || '/public/assets/img/anonymous/anonymous.jpeg'} alt={trainer.name} className="lesson-trainer-image" />
+                      <img
+                        src={
+                          trainer.image ||
+                          "/public/assets/img/anonymous/anonymous.jpeg"
+                        }
+                        alt={trainer.name}
+                        className="lesson-trainer-image"
+                      />
                       <div className="lesson-trainer-info">
                         <p>{trainer.name}</p>
                         <p>{lesson.date}</p>
@@ -82,5 +100,3 @@ const TraineeLessonHistory = () => {
 };
 
 export default TraineeLessonHistory;
-
-
