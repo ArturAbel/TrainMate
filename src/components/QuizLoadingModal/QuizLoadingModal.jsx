61fb643641dc8ae5646073a67010e94431fc1959
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2024-07-31 17:12:55 +0300

    Done wiht making the loader component and implement it in the get started quiz

--- src/components/QuizLoadingModal/QuizLoadingModal.jsx
+++ src/components/QuizLoadingModal/QuizLoadingModal.jsx
@@ -1,10 +1,11 @@
+import Loader from '../Loader/Loader';
 import './QuizLoadingModal.css';
 
 const QuizLoadingModal = () => {
   return (
     <div className="loading-quiz-modal">
       <div className="loader-container">
-        <div className="loader-quiz"></div>
+        <Loader/>
       </div>
       <div className='loading-quiz-modal-text-container'>
         <h1>Loading matched trainers for you ...</h1>
